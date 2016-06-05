4c90436bbc2ca4f8ed8ab65e4e480679b3aab8e4
bind <douglasjbinder@gmail.com>
2016-06-05 11:46:45 -0400

    trimmed dotfile, udpated prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -68,10 +68,10 @@ rb_prompt() {
 }
 
 directory_name() {
-  echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
+  echo "%{$fg_bold[cyan]%}%1/%{$reset_color%}"
 }
 
-export PROMPT=$'\n$(rb_prompt)in $(directory_name) $(git_dirty)$(need_push)\n› '
+export PROMPT=$'$(directory_name) $(git_dirty)$(need_push)\n%{$fg_bold[yellow]%}⚡%{$reset_color%} '
 set_prompt () {
   export RPROMPT="%{$fg_bold[cyan]%}%{$reset_color%}"
 }
