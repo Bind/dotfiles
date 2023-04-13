79e43ddd0d63d46be0c8a655ed88121925d15667
bind <douglasjbinder@gmail.com>
2023-04-13 11:31:14 -0500

    feat: myip alias

--- aliases/alias.sh
+++ aliases/alias.sh
@@ -2,3 +2,4 @@ alias edit='code'
 alias Edit='code'
 alias m='git checkout master'
 alias gpum='git pull upstream --rebase master'
+alias myip="ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'"
